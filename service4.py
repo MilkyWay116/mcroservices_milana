e52381a84c9d1129db3f177975cdf4a508b78a08
MilkyWay116 <116482806+MilkyWay116@users.noreply.github.com>
2023-03-09 06:21:50 +0600

    Update service4.py

--- service4.py
+++ service4.py
@@ -1,10 +1,11 @@
 import time
 from sql_queries import create_table, number_of_pets
+from credentials import conn
 
-create_table()
+create_table(conn)
 
 if __name__ == '__main__':
     while True:
-        number = number_of_pets()
+        number = number_of_pets(conn)
         print("Число пациентов-животных:", str(number))
         time.sleep(10)
